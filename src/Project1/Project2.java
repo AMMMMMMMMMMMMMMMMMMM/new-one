e2f9691d68264e5761fe008d1bd9492f3c241874
AkshayMude <AkshayMude@VTLP110>
2022-11-18 16:20:32 +0530

    second

--- src/Project1/Project2.java
+++ src/Project1/Project2.java
@@ -5,6 +5,8 @@ public class Project2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My second project");
+		System.out.println("My second code");
+		
 	}
 
 }
